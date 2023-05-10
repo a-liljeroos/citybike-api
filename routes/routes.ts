ea7926fa6cd50838c55c9e19fb4623b2d99b320d
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2023-05-10 13:59:13 +0300

    get all stations endpoint

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,7 +1,37 @@
 import { Router, Request, Response } from "express";
+import { Pool, QueryResult } from "pg";
+import { Station, Journey, Count } from "../Types";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const routes = Router();
 
-routes.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: process.env.DATABASE_PASS,
+  database: "citybike",
+});
+
+// returns all stations => {station[]}
+routes.get("/stations/all", (req: Request, res: Response) => {
+  pool.connect((err, client, release) => {
+    if (err) {
+      console.error("Error connecting to the database: ", err);
+      return;
+    }
+
+    client.query(
+      "SELECT * FROM station",
+      (err, results: QueryResult<Station>) => {
+        if (err) {
+          console.error("Error executing query: ", err);
+          return;
+        }
+        res.status(200).json(results.rows);
+        release();
+      }
+    );
+  });
 });
