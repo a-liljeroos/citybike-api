b3a406e40b1ec80da058776a4cb21b51c9242aad
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2023-05-10 15:07:12 +0300

    added journeys endpoint

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import { corsOptions } from "./options";
 import { stationRoutes } from "./routes/stations";
+import { journeyRoutes } from "./routes/journeys";
 
 dotenv.config();
 const port = process.env.PORT;
@@ -12,6 +13,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use("/stations", stationRoutes);
+app.use("/journeys", journeyRoutes);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ routes/journeys.ts
@@ -0,0 +1,65 @@
+import { Router, Request, Response } from "express";
+import { QueryResult } from "pg";
+import { Journey, Count } from "../Types";
+import { pool } from "../options";
+const Joi = require("joi");
+
+export const journeyRoutes = Router();
+
+// returns journeys by page number
+journeyRoutes.get("/pages", (req: Request, res: Response) => {
+  const schema = Joi.object().keys({
+    page: Joi.number().min(1).required(),
+  });
+
+  if (schema.validate(req.query).error) {
+    res.status(422).send(schema.validate(req.query.page).error.details);
+    return;
+  } else {
+    pool.connect((err, client, release) => {
+      if (err) {
+        console.error("Error connecting to the database: ", err);
+        return;
+      }
+      client.query(
+        "SELECT COUNT(*) FROM journey;",
+        (err, results: QueryResult<Count>) => {
+          if (err) {
+            console.error("Error executing query: ", err);
+            return;
+          }
+          const journeyAmount: number = results.rows[0].count;
+          const pageSize: number = 30;
+          const pageNumber: number = Math.ceil(Number(req.query.page));
+
+          const id1 = pageNumber * pageSize - pageSize;
+          const id2 = id1 + pageSize - 1;
+          client.query(
+            "SELECT * FROM journey WHERE id BETWEEN $1 AND $2",
+            [id1, id2],
+            (err, results: QueryResult<Journey>) => {
+              if (err) {
+                console.error("Error executing query: ", err);
+                res.status(400).json({ error: "No results" });
+                return;
+              }
+
+              const journeys = results.rows;
+
+              if (journeys.length === 0) {
+                res.status(400).json({ error: "No results" });
+              }
+              res.status(200).json({
+                currentPage: pageNumber,
+                totalJourneys: journeyAmount,
+                journeys: journeys,
+                pageSize: pageSize,
+              });
+              release();
+            }
+          );
+        }
+      );
+    });
+  }
+});
