4158ff4772023ce2a353e2b9b8c77f528cf29b2f
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2023-05-10 14:23:47 +0300

    moved pg pool in options and separated routes

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import { corsOptions } from "./options";
-import { routes } from "./routes/routes";
+import { stationRoutes } from "./routes/stations";
 
 dotenv.config();
 const port = process.env.PORT;
@@ -11,7 +11,7 @@ const app: Express = express();
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use("/", routes);
+app.use("/stations", stationRoutes);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- routes/stations.ts
+++ routes/stations.ts
@@ -1,27 +1,17 @@
 import { Router, Request, Response } from "express";
-import { Pool, QueryResult } from "pg";
-import { Station, Journey, Count } from "../Types";
-import dotenv from "dotenv";
-dotenv.config();
+import { QueryResult } from "pg";
+import { Station } from "../Types";
+import { pool } from "../options";
 
-export const routes = Router();
-
-const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: process.env.DATABASE_PASS,
-  database: "citybike",
-});
+export const stationRoutes = Router();
 
 // returns all stations => {station[]}
-routes.get("/stations/all", (req: Request, res: Response) => {
+stationRoutes.get("/all", (req: Request, res: Response) => {
   pool.connect((err, client, release) => {
     if (err) {
       console.error("Error connecting to the database: ", err);
       return;
     }
-
     client.query(
       "SELECT * FROM station",
       (err, results: QueryResult<Station>) => {
