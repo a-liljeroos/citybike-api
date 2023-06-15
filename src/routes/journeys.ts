3efc321e13fc681ccc5e925e5e0443bf6fc0e113
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2023-06-15 12:34:05 +0300

    changed journeys endpoint to use TypeORM

--- src/routes/journeys.ts
+++ src/routes/journeys.ts
@@ -1,13 +1,12 @@
 import { Router, Request, Response } from "express";
-import { QueryResult } from "pg";
-import { TJourney, Count } from "../Types";
-import { pool } from "../options";
+import { journeyRepository } from "../data-source";
+
 const Joi = require("joi");
 
 export const journeyRoutes = Router();
 
 // returns journeys by page number
-journeyRoutes.get("/pages", (req: Request, res: Response) => {
+journeyRoutes.get("/pages", async (req: Request, res: Response) => {
   const schema = Joi.object().keys({
     page: Joi.number().min(1).required(),
   });
@@ -17,63 +16,39 @@ journeyRoutes.get("/pages", (req: Request, res: Response) => {
     // WRONG PARAMETERS RESPONSE
     res.status(422).send(schema.validate(req.query.page).error.details);
     return;
-  } else {
-    pool.connect((err, client, release) => {
-      if (err) {
-        console.error("Error connecting to the database: ", err);
-        return;
-      }
-      //
-      // HOW MANY JOURENYS IN THE DATABASE
-      //
-      client.query(
-        "SELECT COUNT(*) FROM journey;",
-        (err, results: QueryResult<Count>) => {
-          if (err) {
-            console.error("Error executing query: ", err);
-            return;
-          }
-          const totalJourneys: number = results.rows[0].count;
-          const pageSize: number = 30;
-          const pageNumber: number = Math.ceil(Number(req.query.page));
+  }
 
-          const id1 = pageNumber * pageSize - pageSize;
-          const id2 = id1 + pageSize - 1;
-          //
-          // SELECT A RANGE OF JOURNEYS FOR THE PAGE
-          //
-          client.query(
-            "SELECT * FROM journey WHERE id BETWEEN $1 AND $2",
-            [id1, id2],
-            (err, results: QueryResult<TJourney>) => {
-              if (err) {
-                console.error("Error executing query: ", err);
-                res.status(400).json({ error: "No results" });
-                return;
-              }
+  try {
+    // CALCULATE PAGINATION VALUES
+    const pageSize = 30;
+    const pageNumber = Math.ceil(Number(req.query.page));
+    const skip = (pageNumber - 1) * pageSize;
 
-              const journeys = results.rows;
-              //
-              // NO RESULTS RESPONSE
-              if (journeys.length === 0) {
-                res.status(400).json({ error: "No results" });
-              }
-              //
-              // PAGE RESPONSE
-              res.status(200).json({
-                journeys: journeys,
-                pagination: {
-                  currentPage: pageNumber,
-                  pageSize: pageSize,
-                  totalJourneys: totalJourneys,
-                  totalPages: Math.ceil(totalJourneys / 30),
-                },
-              });
-              release();
-            }
-          );
-        }
-      );
+    // GET THE TOTAL NUMBER OF JOURNEYS
+    const totalJourneys = await journeyRepository.count();
+
+    // FIND THE JOURNEYS FOR THE GIVEN PAGE
+    const journeys = await journeyRepository.find({
+      skip,
+      take: pageSize,
     });
+
+    if (journeys.length === 0) {
+      return res.status(400).json({ error: "No results" });
+    }
+
+    // PAGE RESPONSE
+    res.status(200).json({
+      journeys,
+      pagination: {
+        currentPage: pageNumber,
+        pageSize,
+        totalJourneys,
+        totalPages: Math.ceil(totalJourneys / pageSize),
+      },
+    });
+  } catch (error) {
+    console.error("Error executing query: ", error);
+    res.status(400).json({ error: "No results" });
   }
 });
