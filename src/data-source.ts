f252a1a459c3505873cc40402f355ee836524dc9
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2023-06-23 12:31:02 +0300

    added test database

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,4 +1,4 @@
-import { DataSource } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { Station, Journey } from "./entities/index";
 require("dotenv").config();
 
@@ -16,13 +16,39 @@ export const AppDataSource = new DataSource({
   subscribers: [],
 });
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((error) => {
-    console.error("Error during Data Source initialization:", error);
-  });
+export const TestDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: process.env.DATABASE_PASS,
+  database: "citybiketest",
+  synchronize: false,
+  logging: false,
+  entities: [Station, Journey],
+  migrations: [],
+  subscribers: [],
+});
 
-export const stationRepository = AppDataSource.getRepository(Station);
-export const journeyRepository = AppDataSource.getRepository(Journey);
+if (process.env.NODE_ENV !== "test") {
+  AppDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((error) => {
+      console.error("Error during Data Source initialization:", error);
+    });
+}
+
+let stationRepository: Repository<Station>;
+let journeyRepository: Repository<Journey>;
+
+if (process.env.NODE_ENV !== "test") {
+  stationRepository = AppDataSource.getRepository(Station);
+  journeyRepository = AppDataSource.getRepository(Journey);
+} else {
+  stationRepository = TestDataSource.getRepository(Station);
+  journeyRepository = TestDataSource.getRepository(Journey);
+}
+
+export { stationRepository, journeyRepository };
